350d1599c53bf3b5b6824ac0ae67ce36c7a4b68b
Artsiom Kushniarou <kushnerovartem@gmail.com>
2019-07-15 16:04:48 +0300

    Add album admin

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -1,7 +1,7 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-from .models import Song
+from .models import Song, Album
 from .models import db
 
 admin = Admin()
@@ -10,3 +10,4 @@ admin = Admin()
 def init_admin(app):
     admin.init_app(app)
     admin.add_view(ModelView(Song, db.session))
+    admin.add_view(ModelView(Album, db.session))
